1e5b00aed87dbe92e6bae50a0f54385f616530b2
Amrender9798 <64745853+Amrender9798@users.noreply.github.com>
2024-01-06 12:27:11 +0530

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/issueTracker');
+     await mongoose.connect(
+      "mongodb+srv://amrender9798:29I3J4AVh1FDhJKT@cluster0.sf4abm3.mongodb.net/issueTracker");
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
